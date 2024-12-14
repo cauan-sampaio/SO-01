d6ec534b69f57e301a63e5550637aa8a9f4daeff
cauan-sampaio <cauan@DESKTOP-PJEJOHF.>
2024-12-14 19:51:59 -0300

    solução2

--- Q3/writters2.c
+++ Q3/writters2.c
@@ -4,12 +4,11 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-#define NUM_READERS 10
+#define NUM_READERS 100
 #define NUM_WRITERS 1
 
 pthread_mutex_t mutex;
-pthread_mutex_t db;
-pthread_mutex_t writer_mutex;
+sem_t db;
 int leitor_lendo = 0;
 int escritores_esperando = 0;
 
@@ -18,72 +17,75 @@ void usar_banco(void);
 void pensando_nos_dados(void);
 void escrever_no_banco(void);
 
-void leitor() {
+void *leitor(void *arg) {
     while (1) {
-        pthread_mutex_lock(&writer_mutex);
         pthread_mutex_lock(&mutex);
-        if (escritores_esperando > 0) {
+        // Bloqueia leitores se escritores estão esperando
+        while (escritores_esperando > 0) {
             pthread_mutex_unlock(&mutex);
-            pthread_mutex_unlock(&writer_mutex);
-            usleep(100);  // Aguarda brevemente para dar chance aos escritores
-            continue;
+            usleep(100);  // Pequena espera para evitar ocupação de CPU
+            pthread_mutex_lock(&mutex);
         }
-        leitor_lendo += 1;
-        if (leitor_lendo == 1)
-            pthread_mutex_lock(&db);
+
+        leitor_lendo++;
+        if (leitor_lendo == 0) {
+            sem_wait(&db); // Primeiro leitor bloqueia a região crítica
+        }
         pthread_mutex_unlock(&mutex);
-        pthread_mutex_unlock(&writer_mutex);
 
         ler_base_de_dados();
 
         pthread_mutex_lock(&mutex);
-        leitor_lendo -= 1;
-        if (leitor_lendo == 0)
-            pthread_mutex_unlock(&db);
+        leitor_lendo--;
+        if (leitor_lendo == 1) {
+            sem_post(&db); // Último leitor libera a região crítica
+        }
         pthread_mutex_unlock(&mutex);
 
         usar_banco();
     }
 }
 
-void escritor() {
+void *escritor(void *arg) {
     while (1) {
         pensando_nos_dados();
-        pthread_mutex_lock(&writer_mutex);
-        escritores_esperando += 1;
-        pthread_mutex_unlock(&writer_mutex);
 
-        pthread_mutex_lock(&db);
+        pthread_mutex_lock(&mutex);
+        escritores_esperando++;
+        printf("Escritores esperando: %d\n", escritores_esperando);
+        pthread_mutex_unlock(&mutex);
+
+        sem_wait(&db); // Escritor bloqueia a região crítica
         escrever_no_banco();
-        pthread_mutex_unlock(&db);
+        sem_post(&db); // Escritor libera a região crítica
 
-        pthread_mutex_lock(&writer_mutex);
-        escritores_esperando -= 1;
-        pthread_mutex_unlock(&writer_mutex);
+        pthread_mutex_lock(&mutex);
+        escritores_esperando--;
+        pthread_mutex_unlock(&mutex);
     }
 }
 
 void ler_base_de_dados() {
-    int tempo_leitura = rand() % 5;
-    printf("Usuario lendo banco de dados. Total de %d leitores lendo agora\n", leitor_lendo);
+    int tempo_leitura = rand() % 3 + 1;
+    printf("Leitor lendo banco de dados. Total de leitores: %d\n", leitor_lendo);
     sleep(tempo_leitura);
 }
 
 void usar_banco() {
-    int tempo_uso = rand() % 15;
-    printf("Usuario utilizando conhecimento do banco de dados.\n");
+    int tempo_uso = rand() % 5 + 1;
+    printf("Leitor utilizando dados do banco.\n");
     sleep(tempo_uso);
 }
 
 void pensando_nos_dados() {
-    int tempo_pensar = rand() % 10;
-    printf("Escritor pensando no que escrever\n");
-    sleep(tempo_pensar);
+    int esperando = rand() % 4 + 1;
+    // printf("Escritores esperando: %d\n", escritores_esperando);
+    sleep(esperando);
 }
 
 void escrever_no_banco() {
-    int tempo_escrever = rand() % 10;
-    printf("Escritor escrevendo no banco de dados\n");
+    int tempo_escrever = rand() % 3 + 1;
+    printf("Escritor escrevendo no banco de dados.\n");
     sleep(tempo_escrever);
 }
 
@@ -91,21 +93,20 @@ int main() {
     pthread_t readers[NUM_READERS], writers[NUM_WRITERS];
     int i;
 
-    pthread_mutex_init(&db, NULL);
     pthread_mutex_init(&mutex, NULL);
-    pthread_mutex_init(&writer_mutex, NULL);
+    sem_init(&db, 0, 1);
 
     // Criando threads de leitores
     for (i = 0; i < NUM_READERS; i++) {
-        pthread_create(&readers[i], NULL, (void *) leitor, NULL);
+        pthread_create(&readers[i], NULL, leitor, NULL);
     }
 
     // Criando threads de escritores
     for (i = 0; i < NUM_WRITERS; i++) {
-        pthread_create(&writers[i], NULL, (void *) escritor, NULL);
+        pthread_create(&writers[i], NULL, escritor, NULL);
     }
 
-    // Aguarda as threads terminarem
+    // Aguardando threads
     for (i = 0; i < NUM_READERS; i++) {
         pthread_join(readers[i], NULL);
     }
@@ -113,6 +114,9 @@ int main() {
     for (i = 0; i < NUM_WRITERS; i++) {
         pthread_join(writers[i], NULL);
     }
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&db);
 
     return 0;
 }
